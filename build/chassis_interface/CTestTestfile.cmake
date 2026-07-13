b60f5fcd3003afed86bde5afe41caa1dca8422b0
武理仔9号 <2778098885@qq.com>
2026-07-13 14:46:56 +0800

    清理: 移除误上传的IDE配置、编译产物、临时文件、文档

--- build/chassis_interface/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wa/inspection/巡迹导航盒子/test_ws/src/chassis_interface
-# Build directory: /home/wa/inspection/巡迹导航盒子/test_ws/build/chassis_interface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
